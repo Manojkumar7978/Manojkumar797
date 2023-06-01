dba902392bcd95c881006738bef7c6e741fbc0c0
Manoj Kumar Padhy <manojkumarpadhyms@gmail.com>
2023-06-01 08:11:50 +0530

    tool

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -6,7 +6,7 @@ import MANOJ_PADHY_RESUME from './MANOJ_PADHY_RESUME.pdf'
 import pic from './image.jpg'
 const onClickhandler = () => {
     return window.open(
-        "https://drive.google.com/file/d/1HhD1HLlvqN9gru7-09czN3yvpvIvYQh2/view?usp=sharing",
+        "https://drive.google.com/file/d/1MCx2RFX-4V10NtrGQujSZusBwiQiBKSc/view?usp=sharing",
     );
 };
 
