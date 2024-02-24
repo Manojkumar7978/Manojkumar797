f6e463e91a28e63754fba359326c068b7e1b1f37
Manoj Kumar Padhy <122422501+Manojkumar7978@users.noreply.github.com>
2024-02-24 11:26:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+  import './App.css';
 import { Box } from '@chakra-ui/react';
 import Navbar from './components/nav';
 import Home from "./components/home";
@@ -6,6 +6,7 @@ import About from './components/about';
 import Skills from './components/skills';
 import Projects from './components/projects';
 import Contact from './components/contact';
+import { techstack, tools } from './assests/data';
 
 function App() {
   return (
@@ -14,7 +15,9 @@ function App() {
       <Box bg={'gray.100'}>
         <section id="home"><Home /></section>
         <section id="about" class="about section"><About /></section>
-        <section id="skills"><Skills /></section>
+       <section id="skills" ><Skills techstack={techstack} heading={"Skills"}/>
+        <Skills techstack={tools} heading={"Tools"}/>
+        </section>
         <section id="projects"><Projects /></section>
         <section id="contact"><Contact /></section>
       </Box>
